861afe22c5271e1dfdb30385f79c5143cb406bdb
Pedro <2pedro8@gmail.com>
2015-11-26 20:22:51 -0200

    torna css responsivo. falta o script (nao sei qual o erro)

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 $(document).ready(function(){
-  $('.botao-menu').click(function(){
-    var img = $(this).data("img");
-    $('div.container-design').hide();
-    $('div.container-design[data-img="'+img+'"]').show();
-    $('div.botao-menu').attr('data-onoff','off');
-    $(this).attr('data-onoff','on');
-  });
 
-  $("img").click(function(){
-    var src = $(this).attr('src');
-    var width = $(this).css('width');
-    var scale = $(this).attr('data-scale');
-    $('#fundo').css({'display':'block'});
-    $('#fundo img').attr('src',src).css({'width':width,'-ms-transform':'scale('+scale+')','-webkit-transform':' scale('+scale+')','transform':'scale('+scale+')','z-index':'3'});
+      $('.botao-menu').click(function(){
+        if (screen.availWidth > 992){
+          var img = $(this).data("img");
+          $('div.container-design').hide();
+          $('div.container-design[data-img="'+img+'"]').show();
+          $('div.botao-menu').attr('data-onoff','off');
+          $(this).attr('data-onoff','on');
+        }
+      });
 
-  });
+      $("img").click(function(){
+        if (screen.availWidth>992){
+          var src = $(this).attr('src');
+          var width = $(this).css('width');
+          var scale = $(this).attr('data-scale');
+          $('#fundo').css({'display':'block'});
+          $('#fundo img').attr('src',src).css({'width':width,'-ms-transform':'scale('+scale+')','-webkit-transform':' scale('+scale+')','transform':'scale('+scale+')','z-index':'3'});
+        }
+      });
 
-  $("#fundo").click(function(){
-    $('img').css({'-ms-transform':'none','-webkit-transform':'none','transform':'none','z-index':'2'});
-    $(this).css({'display':'none'});
-  })
+      $("#fundo").click(function(){
+        if (screen.availWidth > 992){
+          $('img').css({'-ms-transform':'none','-webkit-transform':'none','transform':'none','z-index':'2'});
+          $(this).css({'display':'none'});
+        }  
+      });
 });
