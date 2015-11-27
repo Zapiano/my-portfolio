bd1578ad7c3b6db7b534975e3269718f092d61f0
Pedro <2pedro8@gmail.com>
2015-11-27 20:11:49 -0200

    tentativa de consertar bug no ipad (script nao roda)

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 $(document).ready(function(){
       $('.botao-menu').click(function(){
-        if (screen.availWidth > 992){
-          var img = $(this).data("img");
-          $('div.container-design').hide();
-          $('div.container-design[data-img="'+img+'"]').show();
-          $('div.botao-menu').attr('data-onoff','off');
-          $(this).attr('data-onoff','on');
-        }
+        var img = $(this).data("img");
+        $('div.container-design').hide();
+        $('div.container-design[data-img="'+img+'"]').show();
+        $('div.botao-menu').attr('data-onoff','off');
+        $(this).attr('data-onoff','on');
       });
 
       $(".container-design img").click(function(){
-        if (screen.availWidth>992){
-          var src = $(this).attr('src');
-          var width = $(this).css('width');
-          var scale = $(this).attr('data-scale');
-          $('#fundo').css({'display':'block'});
-          $('#fundo img').attr('src',src).css({'width':width,'-ms-transform':'scale('+scale+')','-webkit-transform':' scale('+scale+')','transform':'scale('+scale+')','z-index':'3'});
-        }
+        var src = $(this).attr('src');
+        var width = $(this).css('width');
+        var scale = $(this).attr('data-scale');
+        $('#fundo').css({'display':'block'});
+        $('#fundo img').attr('src',src).css({'width':width,'-ms-transform':'scale('+scale+')','-webkit-transform':' scale('+scale+')','transform':'scale('+scale+')','z-index':'3'});
       });
 
       $("#fundo").click(function(){
-        if (screen.availWidth > 992){
-          $('.contaier-design img').css({'-ms-transform':'none','-webkit-transform':'none','transform':'none','z-index':'2'});
-          $(this).css({'display':'none'});
-        }  
+        $('.contaier-design img').css({'-ms-transform':'none','-webkit-transform':'none','transform':'none','z-index':'2'});
+        $(this).css({'display':'none'});  
       });
 });
